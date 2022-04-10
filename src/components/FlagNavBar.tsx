42956a7b7fe5065558ae61ca760f8cfef5387e37
CaioAANolasco <caioaanolasco@gmail.com>
2022-04-10 20:36:36 -0300

    Initial files for flags filtering nav bar

---
+++ src/components/FlagNavBar.tsx
@@ -0,0 +1,13 @@
+import { Card, Button } from "@blueprintjs/core";
+
+const FlagNavBar = () => {
+	return (
+		<div>
+			<Card>
+				<Button text="Sets" icon="chevron-left" minimal={true}></Button>
+			</Card>
+		</div>
+	);
+};
+
+export default FlagNavBar;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,12 @@
 import MenuBar from "./MenuBar";
 import FilterProperties from "./FilterProperties";
+import FlagNavBar from "./FlagNavBar";
 import "./styles/sidebar.scss";
 
 const Sidebar = () => {
 	return (
 		<div className="sidebar-container">
+			<FlagNavBar />
 			<FilterProperties />
 			<MenuBar />
 		</div>

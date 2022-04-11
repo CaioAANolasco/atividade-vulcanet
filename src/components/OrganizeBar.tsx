d5ef5c6cd11e724ca53ed8a41b4992cc50c4f7e7
CaioAANolasco <caioaanolasco@gmail.com>
2022-04-11 12:22:17 -0300

    Added buttons to org bar and added spacing between elements

--- src/components/OrganizeBar.tsx
+++ src/components/OrganizeBar.tsx
@@ -1,16 +1,28 @@
-import { InputGroup, Navbar, NavbarGroup } from "@blueprintjs/core";
+import { Button, InputGroup, Navbar, NavbarGroup } from "@blueprintjs/core";
 import "./styles/organize-styles/organize-bar.scss";
 
 const OrganizeBar = () => {
 	return (
 		<Navbar className="organize-bar">
-			<NavbarGroup>
+			<NavbarGroup className="search-group" align="left">
 				<InputGroup
 					leftIcon="search"
 					round={true}
 					placeholder="Search for assets"
 					className="organize-bar-search"
 				></InputGroup>
+				<Button
+					text="List"
+					icon="list"
+					minimal={true}
+					active={true}
+					intent="primary"
+				/>
+				<Button
+					text="Group by trader"
+					icon="grid-view"
+					minimal={true}
+				/>
 			</NavbarGroup>
 		</Navbar>
 	);

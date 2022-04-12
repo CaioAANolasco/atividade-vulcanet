b57215cb028fe6603e4a9ff2a8897332330e8e0f
CaioAANolasco <caioaanolasco@gmail.com>
2022-04-12 19:35:55 -0300

    Changed actives state variable name for clarity

--- src/app/reducers/activesReducer.ts
+++ src/app/reducers/activesReducer.ts
@@ -5,22 +5,27 @@ import producer from "immer";
 
 const jsonData: ActiveInformation[] = require("../../ACTIVEINFO.json");
 const initialState = jsonData;
-const activesReducer = producer((state = initialState, action: Action) => {
-	switch (action.type) {
-		case ActionType.FETCH_ACTIVES:
-			return state;
-		case ActionType.FILTER_TAG:
-			//sort by price
-			return state;
-		case ActionType.FILTER_NAME:
-			const filterInput = action.payload;
-			let filteredValues = state.filter((active: ActiveInformation) => {
-				return active.activeInfo.toLowerCase().includes(filterInput);
-			});
-			state = filteredValues;
-			return state;
-		default:
-			return state;
+const activesReducer = producer(
+	(activesState = initialState, action: Action) => {
+		switch (action.type) {
+			case ActionType.FETCH_ACTIVES:
+				return activesState;
+			case ActionType.FILTER_TAG:
+				//sort by price
+				return activesState;
+			case ActionType.FILTER_NAME:
+				const filterInput = action.payload;
+				let filteredValues = activesState.filter(
+					(active: ActiveInformation) => {
+						return active.activeInfo
+							.toLowerCase()
+							.includes(filterInput);
+					}
+				);
+				return filteredValues;
+			default:
+				return activesState;
+		}
 	}
-});
+);
 export default activesReducer;

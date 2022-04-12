9d39c27686f76f0077c9c74712b0c548d2ef8eaf
CaioAANolasco <caioaanolasco@gmail.com>
2022-04-12 19:28:24 -0300

    Removed unused counter file

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,9 +1,7 @@
 import activesReducer from "./activesReducer";
 import { combineReducers } from "redux";
-import counterReducer from "../../features/counter/counterSlice";
 
 const reducers = combineReducers({
-	counter: counterReducer,
 	actives: activesReducer,
 });
 

6cba3b87ee6231c42d55f1cfbb143722b6c9b216
CaioAANolasco <caioaanolasco@gmail.com>
2022-04-14 15:54:17 -0300

    Removed unused import from Redux template

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./components/App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
 	<React.StrictMode>
